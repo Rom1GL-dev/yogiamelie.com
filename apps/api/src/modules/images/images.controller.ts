ddff40d8c3dc82937778d9a0b1506a3ed46c87cf
RomainEntreCodeurs <romain@lesentrecodeurs>
2025-06-01 17:50:17 +0200

    feat: add prod

--- apps/api/src/modules/images/images.controller.ts
+++ apps/api/src/modules/images/images.controller.ts
@@ -17,7 +17,7 @@ import { routesV1 } from '../../config/app.routes';
 
 @Controller(routesV1.version)
 export class ImagesController {
-  private basePath = 'apps/yogiamelie.com/images';
+  private basePath = 'apps/images';
 
   @Post(routesV1.image.upload)
   @UseInterceptors(
@@ -25,7 +25,7 @@ export class ImagesController {
       storage: diskStorage({
         destination: (req, file, cb) => {
           const category = req.params.category;
-          const uploadPath = path.join('apps/yogiamelie.com/images', category);
+          const uploadPath = path.join('apps/images', category);
           fs.mkdirSync(uploadPath, { recursive: true });
           cb(null, uploadPath);
         },
