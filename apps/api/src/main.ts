40ef50f2f305e501ae1fa872c8651fad9857f2fc
RomainEntreCodeurs <romain@lesentrecodeurs>
2025-06-06 15:40:17 +0200

    fix:

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,17 +3,29 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn', 'debug', 'verbose'],
   });
+
   const configService = app.get(ConfigService);
 
   const appUrlEnv = configService.get<string>('APP_URL');
+  const uploadPath = configService.get<string>('UPLOAD_PATH') ?? '/tmp/uploads';
 
   if (!appUrlEnv) {
     throw new Error('APP_URL is required to allow CORS');
+  }
+
+  if (!uploadPath) {
+    throw new Error('UPLOAD_PATH is not defined');
+  }
+
+  // S'assurer que le dossier d'upload existe
+  if (!fs.existsSync(uploadPath)) {
+    fs.mkdirSync(uploadPath, { recursive: true });
   }
 
   const allowedOrigins = appUrlEnv.split(',').map((url) => url.trim());
@@ -43,5 +55,6 @@ async function bootstrap() {
 
   const port = parseInt(configService.get('PORT') ?? '3000', 10);
   await app.listen(port);
+  console.log(`🚀 App running on http://localhost:${port}`);
 }
 void bootstrap();
--- apps/api/src/modules/images/images.controller.ts
+++ apps/api/src/modules/images/images.controller.ts
@@ -14,10 +14,16 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { Response as ExpressResponse } from 'express';
 import { routesV1 } from '../../config/app.routes';
+import { ConfigService } from '@nestjs/config';
 
 @Controller(routesV1.version)
 export class ImagesController {
-  private basePath = '/home/debian/apps/images/';
+  private readonly basePath: string;
+
+  constructor(private readonly configService: ConfigService) {
+    this.basePath =
+      this.configService.get<string>('UPLOAD_PATH') ?? '/tmp/uploads';
+  }
 
   @Post(routesV1.image.upload)
   @UseInterceptors(
@@ -25,7 +31,9 @@ export class ImagesController {
       storage: diskStorage({
         destination: (req, file, cb) => {
           const category = req.params.category;
-          const uploadPath = path.join('/home/debian/apps/images/', category);
+          const uploadBasePath = process.env.UPLOAD_PATH || '/tmp/uploads';
+          const uploadPath = path.join(uploadBasePath, category);
+
           fs.mkdirSync(uploadPath, { recursive: true });
           cb(null, uploadPath);
         },
