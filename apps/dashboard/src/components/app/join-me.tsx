86e7043531995357140104367608b6e1282e30f3
RomainEntreCodeurs <romain@lesentrecodeurs>
2025-06-30 21:53:04 +0200

    fix(*):

--- apps/dashboard/src/components/app/join-me.tsx
+++ apps/dashboard/src/components/app/join-me.tsx
@@ -10,32 +10,62 @@ import 'swiper/css/pagination';
 import { Navigation, Pagination } from 'swiper/modules';
 import { EVENT_TYPE } from '@/features/events/types/events.type.ts';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { useState } from 'react';
 
 export const JoinMe = observer(() => {
   const { eventStore } = useStores();
-
+  const [allEvents, setAllEvents] = useState<boolean>(false);
   if (eventStore.events.length === 0) return null;
 
   return (
     <LayoutApp title={'Rejoins-moi !'} background={'#d5ddcb'}>
-      {
-        <div className="relative" data-aos="fade-up" data-aos-delay="350">
-          <button
-            className="swiper-button-prev-event absolute top-1/2 -left-6 z-10 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-gray-300 text-white hover:bg-gray-400"
-            aria-label="Précédent"
-            style={{ left: '-40px' }}
-          >
-            <ChevronLeft />
-          </button>
+      <div className="relative" data-aos="fade-up" data-aos-delay="350">
+        {!allEvents && (
+          <>
+            <button
+              className="swiper-button-prev-event absolute top-1/2 -left-6 z-10 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-gray-300 text-white hover:bg-gray-400"
+              aria-label="Précédent"
+              style={{ left: '-40px' }}
+            >
+              <ChevronLeft />
+            </button>
 
-          <button
-            className="swiper-button-next-event absolute top-1/2 -right-6 z-10 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-gray-300 text-white hover:bg-gray-400"
-            aria-label="Suivant"
-            style={{ right: '-40px' }}
-          >
-            <ChevronRight />
-          </button>
+            <button
+              className="swiper-button-next-event absolute top-1/2 -right-6 z-10 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-gray-300 text-white hover:bg-gray-400"
+              aria-label="Suivant"
+              style={{ right: '-40px' }}
+            >
+              <ChevronRight />
+            </button>
+          </>
+        )}
 
+        {allEvents ? (
+          <div className={'grid grid-cols-3'}>
+            {eventStore.filteredEvents
+              .filter(
+                (event) =>
+                  event.type === EVENT_TYPE.FUTURE ||
+                  event.type === EVENT_TYPE.ON_GOING
+              )
+              .sort(
+                (a, b) =>
+                  new Date(a.startDate).getTime() -
+                  new Date(b.startDate).getTime()
+              )
+              .map((event) => (
+                <SwiperSlide key={event.id}>
+                  <div
+                    className="md:p-4 lg:p-6"
+                    data-aos="fade-up"
+                    data-aos-delay="350"
+                  >
+                    <EventCard event={event} />
+                  </div>
+                </SwiperSlide>
+              ))}
+          </div>
+        ) : (
           <Swiper
             modules={[Navigation, Pagination]}
             navigation={{
@@ -74,8 +104,16 @@ export const JoinMe = observer(() => {
                 </SwiperSlide>
               ))}
           </Swiper>
+        )}
+        <div className="mt-4 flex justify-center">
+          <button
+            className="mt-5 cursor-pointer rounded-md border border-black bg-[#eed7c1] px-4 py-2 text-black hover:bg-[#e0c1aa]"
+            onClick={() => setAllEvents(!allEvents)}
+          >
+            {allEvents ? 'Voir moins' : 'Voir plus'}
+          </button>
         </div>
-      }
+      </div>
     </LayoutApp>
   );
 });
